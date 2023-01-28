20d018c64ec29e1b1bfe7321d4a4bccb2773d761
Grzesiek92PL <grzesiek316316@gmail.com>
2023-01-28 14:36:55 +0100

    html

--- src/main/java/com/example/projectfinal/contact/contactController.java
+++ src/main/java/com/example/projectfinal/contact/contactController.java
@@ -1,7 +1,8 @@
 package com.example.projectfinal.contact;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class contactController {
 
     @GetMapping("/contact")
